db2dc3d8b8c1221e846f75c7eda74a18004007d5
Nishi <ngudai999@gmail.com>
2020-12-24 16:13:12 +0700

    First GraphQL Schema

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const {
+    buildSchema
+} = require('graphql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+
+            type RootMutation {
+                createEvent(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return  ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName.toUpperCase();
+            }
+        },
+        graphiql: true
+    })
+);
+
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {console.log(`Server are running at port ${port}`);});
+app.listen(port, () => {
+    console.log(`Server are running at port ${port}`);
+});
