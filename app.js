55cad3c286c2543d7e58c443c6dbc9aa159caaa0
Nishi <ngudai999@gmail.com>
2020-12-25 16:27:06 +0700

    Test EventOutput Type & EventInput Type

--- app.js
+++ app.js
@@ -3,48 +3,72 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const {
-    buildSchema
+  buildSchema
 } = require('graphql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema: buildSchema(`
-            type RootQuery {
-                events: [String!]!
-            }
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type Event {
+        _id: ID!,
+        title: String!,
+        description: String!,
+        price: Float!,
+        date: String!
+      }
 
-            type RootMutation {
-                createEvent(name: String): String
-            }
+      input EventInput {
+        title: String!,
+        description: String!,
+        price: Float!,
+        date: String!
+      }
 
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-        rootValue: {
-            events: () => {
-                return  ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
-            },
-            createEvent: (args) => {
-                const eventName = args.name;
-                return eventName.toUpperCase();
-            }
-        },
-        graphiql: true
-    })
+      type RootQuery {
+        events: [Event!]!
+      }
+
+      type RootMutation {
+        createEvent(eventInput: EventInput): Event
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        }
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
 );
 
 app.get('/', (req, res, next) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server are running at port ${port}`);
+  console.log(`Server are running at port ${port}`);
 });
